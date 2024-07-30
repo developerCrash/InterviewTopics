f879eeb7814bb73f5e034dc41a547815ba56e655
Anil Kumar G <35560797+developerCrash@users.noreply.github.com>
2024-07-30 16:40:50 +0530

    Update ReplaceUsingRegex.py

--- PythonCode/ReplaceUsingRegex.py
+++ PythonCode/ReplaceUsingRegex.py
@@ -7,6 +7,8 @@ myStr = "Anil kumar is aa witaa apple"
 
 result = pattern.findall(myStr)
 print(result if result else "NO Match")
+## Print the number of times a substring is repeated 
+print(len(result))
 
 result = pattern.sub(replacement_string, myStr)
 print(result)
