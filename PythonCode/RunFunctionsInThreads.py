adc83de6076cdc5f405a0e283ba4c2dea2926888
Anil Kumar G <35560797+developerCrash@users.noreply.github.com>
2024-11-25 10:30:49 +0530

    Update RunFunctionsInThreads.py

--- PythonCode/RunFunctionsInThreads.py
+++ PythonCode/RunFunctionsInThreads.py
@@ -1,1 +1,21 @@
+import threading
+
+def print_num(n):
+    for i in range(n):
+        print(i)
+
+def print_more_num(n):
+    for i in range(n):
+        print(i*i)
+
+
+thread1 = threading.Thread(target=print_num(10))
+thread2 = threading.Thread(target=print_more_num(20))
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
 
