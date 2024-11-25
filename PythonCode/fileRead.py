835030986c3bbf473e2269b68e4e97df352e7d6b
Anil Kumar G <35560797+developerCrash@users.noreply.github.com>
2024-11-25 11:30:33 +0530

    Update fileRead.py

--- PythonCode/fileRead.py
+++ PythonCode/fileRead.py
@@ -20,3 +20,11 @@ with open('anil.txt', 'r') as f:
     print(content)
     print(f.tell())  # Get the current position (should be 0 initially)
     f.seek(0)  # Move back to the start of the file
+
+
+lines = ["First line\n", "Second line\n", "Third line\n"]
+
+# Open the file in write mode
+with open('example.txt', 'w') as file:
+    file.writelines(lines)
+    file.flush()  # Flushes the internal buffer to disk
