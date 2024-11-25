b24a3c3104617729bc6c5f7f2e76513c98e49dec
Anil Kumar G <35560797+developerCrash@users.noreply.github.com>
2024-11-25 11:03:13 +0530

    Create fileRead.py

---
+++ PythonCode/fileRead.py
@@ -0,0 +1,22 @@
+## Reads entire file 
+with open("anil.txt", "r") as f:
+    content = f.read()
+    print(content)
+
+
+## Read Line by line
+with open("anil.txt", "r") as f:
+    for line in f:
+        print(line.strip())
+
+## Read line by line in a list 
+with open('anil.txt', 'r') as file:
+    lines = f.readlines()
+    print(lines)
+
+## Reading Characters 
+with open('anil.txt', 'r') as f:
+    content = f.read(10)  # Reads the first 10 characters
+    print(content)
+    print(f.tell())  # Get the current position (should be 0 initially)
+    f.seek(0)  # Move back to the start of the file
